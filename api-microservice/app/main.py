76e063cd5bc16651de98b83f60eee0bdef42386b
narodriguezb <benestor96@hotmail.com>
2026-03-02 21:16:00 -0500

    add microservice

---
+++ api-microservice/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI(title="Microservicio Demo")
+
+
+@app.get("/live")
+def liveness():
+    return {"status": "alive", "timestamp": datetime.utcnow().isoformat()}
+
+
+@app.get("/ready")
+def readiness():
+    return {"status": "ready", "timestamp": datetime.utcnow().isoformat()}
+
+
+@app.get("/")
+def root():
+    return {"message": "Microservicio funcionando"}
