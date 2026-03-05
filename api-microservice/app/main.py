d02b19ef9af9c8644fccd7d0c77a08cd9ac6c416
narodriguezb <benestor96@hotmail.com>
2026-03-05 15:48:18 -0500

    add envs

--- api-microservice/app/main.py
+++ api-microservice/app/main.py
@@ -1,24 +1,38 @@
 import time
+import os
 from fastapi import FastAPI
 from datetime import datetime
 
-print("Inicializando app...", flush=True)
-time.sleep(15)
-print("App lista.", flush=True)
+APP_ENV = os.getenv("APP_ENV", "dev")
 
-app = FastAPI(title="Microservicio Demo")
+print(f"Inicializando app... [ambiente: {APP_ENV}]", flush=True)
+time.sleep(15)
+print(f"App lista. [ambiente: {APP_ENV}]", flush=True)
+
+app = FastAPI(title=f"Microservicio Demo - {APP_ENV.upper()}")
 
 
 @app.get("/live")
 def liveness():
-    return {"status": "alive", "timestamp": datetime.utcnow().isoformat()}
+    return {
+        "status": "alive",
+        "env": APP_ENV,
+        "timestamp": datetime.utcnow().isoformat(),
+    }
 
 
 @app.get("/ready")
 def readiness():
-    return {"status": "ready", "timestamp": datetime.utcnow().isoformat()}
+    return {
+        "status": "ready",
+        "env": APP_ENV,
+        "timestamp": datetime.utcnow().isoformat(),
+    }
 
 
 @app.get("/")
 def root():
-    return {"message": "Microservicio funcionando"}
+    return {
+        "message": "Microservicio funcionando",
+        "env": APP_ENV,
+    }
