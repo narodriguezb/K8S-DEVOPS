6fdb8b5f686b04d92f464464e157b312b3797f66
narodriguezb <benestor96@hotmail.com>
2026-03-05 21:33:53 -0500

    prod vtest

--- api-microservice/app/main.py
+++ api-microservice/app/main.py
@@ -26,7 +26,7 @@ def readiness():
     return {
         "status": "ready",
         "env": APP_ENV,
-        "change": "probando cambios para ver todo el flujo v3",
+        "change": "probando cambios para ver todo el flujo produccion testing v1",
         "timestamp": datetime.utcnow().isoformat(),
     }
 
